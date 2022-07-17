6131f7e1502fad9fc2032e57ec8602e4083e0e32
Ilya Akkuzin <gr3yknigh1@gmail.com>
2022-07-17 11:40:38 +0300

    [bootstrap]: update script

--- bootstrap.py
+++ bootstrap.py
@@ -12,23 +12,21 @@ HOME_DIR = os.path.expanduser('~')
 
 with open("packages.json") as f:
     packages: dict = json.load(f)
-
-
+"""
 subprocess.run(["sudo", "pacman", "-Sy", *packages.get("pacman", [])])
+subprocess.run(["python", "-m", "ensurepip"])
+subprocess.run(["pip3", "install", "--upgrade", "pip"])
 subprocess.run(["pip", "install", *packages.get("pip", [])])
 subprocess.run(["sudo", "npm", "install", "--global", *packages.get("npm", [])])
-
-subprocess.run([f"{HOME_DIR}/.dotfiles/scripts/install-rustup.sh"])
-subprocess.run([f"{HOME_DIR}/.dotfiles/scripts/install-omf.sh"])
-subprocess.run([f"{HOME_DIR}/.dotfiles/scripts/install-tpm.sh"])
-subprocess.run([f"{HOME_DIR}/.dotfiles/scripts/chsh2fish.sh"])
-
-subprocess.run(["omf", "install", "nai"])
-
-subprocess.run([f"{HOME_DIR}/.dotfiles/scripts/setup-ssh.sh"])
-
+subprocess.run(["sh", f"{HOME_DIR}/.dotfiles/scripts/install-rustup.sh"])
+subprocess.run(["sh", f"{HOME_DIR}/.dotfiles/scripts/install-omf.sh"])
+subprocess.run(["sh", f"{HOME_DIR}/.dotfiles/scripts/install-tpm.sh"])
+subprocess.run(["fish", "omf", "install", "nai"])
+subprocess.run(["sh", f"{HOME_DIR}/.dotfiles/scripts/setup-ssh.sh"])
+"""
 for pkg in packages.get("stow", []):
-    subprocess.run(
+    subprocess.run([
         "stow", "-S", pkg
-        )
+        ])
 
+# subprocess.run(["sh", f"{HOME_DIR}/.dotfiles/scripts/chsh2fish.sh"])
