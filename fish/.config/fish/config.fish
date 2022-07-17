74104f6665a7841e9846a5cd50bb0fad15743aff
Ilya Akkuzin <gr3yknigh1@gmail.com>
2022-07-17 11:41:14 +0300

    [fish]: add xserver run command

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -17,3 +17,6 @@ alias l1="exa $LS_ALL_CHAR -1 --icons --group-directories-first $argv"
 alias ll="exa $LS_ALL_CHAR -l --icons --group-directories-first $argv"
 alias lt="exa $LS_ALL_CHAR -T --icons --group-directories-first $argv"
 
+if [ $(tty) = "/dev/tty1" ]
+    startx
+end
