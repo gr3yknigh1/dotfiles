7f2c3f2e8ded3561e8ea2e61f45a7a17f8da196f
Ilya Akkuzin <gr3yknigh1@gmail.com>
2022-07-18 21:06:36 +0300

    [fish]: Add tmux aliases

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -17,6 +17,15 @@ alias l1="exa $LS_ALL_CHAR -1 --icons --group-directories-first $argv"
 alias ll="exa $LS_ALL_CHAR -l --icons --group-directories-first $argv"
 alias lt="exa $LS_ALL_CHAR -T --icons --group-directories-first $argv"
 
+# -- Tmux
+
+alias t="tmux"
+alias ta="tmux attach"
+alias tls="tmux ls"
+alias tkr="tmux kill-server"
+alias tks="tmux kill-session"
+
+# -- start x-server
 if [ $(tty) = "/dev/tty1" ]
     startx
 end
