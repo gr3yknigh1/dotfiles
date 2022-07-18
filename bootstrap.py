1e0bd7c71a37a320672d269b21b12805cdfc1544
Ilya Akkuzin <gr3yknigh1@gmail.com>
2022-07-18 21:10:21 +0300

    [nvim]: Remove comments in bootstrap script

--- bootstrap.py
+++ bootstrap.py
@@ -12,7 +12,7 @@ HOME_DIR = os.path.expanduser('~')
 
 with open("packages.json") as f:
     packages: dict = json.load(f)
-"""
+
 subprocess.run(["sudo", "pacman", "-Sy", *packages.get("pacman", [])])
 subprocess.run(["python", "-m", "ensurepip"])
 subprocess.run(["pip3", "install", "--upgrade", "pip"])
@@ -23,10 +23,10 @@ subprocess.run(["sh", f"{HOME_DIR}/.dotfiles/scripts/install-omf.sh"])
 subprocess.run(["sh", f"{HOME_DIR}/.dotfiles/scripts/install-tpm.sh"])
 subprocess.run(["fish", "omf", "install", "nai"])
 subprocess.run(["sh", f"{HOME_DIR}/.dotfiles/scripts/setup-ssh.sh"])
-"""
+
 for pkg in packages.get("stow", []):
     subprocess.run([
         "stow", "-S", pkg
         ])
 
-# subprocess.run(["sh", f"{HOME_DIR}/.dotfiles/scripts/chsh2fish.sh"])
+subprocess.run(["sh", f"{HOME_DIR}/.dotfiles/scripts/chsh2fish.sh"])
