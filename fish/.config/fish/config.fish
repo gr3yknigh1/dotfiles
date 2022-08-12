d3f27b12f5f05626d64fcab501df212797a25229
Ilya Akkuzin <gr3yknigh1@gmail.com>
2022-08-12 11:37:16 +0300

    [fish] update aliases

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,23 +1,19 @@
 if status is-interactive
 end
 
+set EDITOR "nvim"
 
 alias cls="clear"
 alias lg="lazygit"
 
-# -- EDITOR
-set EDITOR "nvim"
 alias e=$EDITOR
 
-# -- CAT
 alias cat="bat"
 
 alias ls="exa $LS_ALL_CHAR --icons --group-directories-first $argv"
 alias l1="exa $LS_ALL_CHAR -1 --icons --group-directories-first $argv"
 alias ll="exa $LS_ALL_CHAR -l --icons --group-directories-first $argv"
 alias lt="exa $LS_ALL_CHAR -T --icons --group-directories-first $argv"
-
-# -- Tmux
 
 alias t="tmux"
 alias tal="tmux attach"
@@ -26,7 +22,9 @@ alias tls="tmux ls"
 alias tkr="tmux kill-server"
 alias tks="tmux kill-session"
 
-# -- start x-server
+
+# start x-server
 if [ $(tty) = "/dev/tty1" ]
     startx
 end
+
