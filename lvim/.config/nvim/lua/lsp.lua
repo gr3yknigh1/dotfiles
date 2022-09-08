56aac36ade5ea336b26091020373e0fa4423a660
Ilya Akkuzin <gr3yknigh1@gmail.com>
2022-09-08 13:36:17 +0300

    [lvim] add move lsp keybindings

--- lvim/.config/nvim/lua/lsp.lua
+++ lvim/.config/nvim/lua/lsp.lua
@@ -3,6 +3,15 @@
 local custom_lsp_attach = function()
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = 0 })
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = 0 })
+  vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, { buffer = 0 })
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { buffer = 0 })
+
+  vim.keymap.set('n', '<Leader>dn', vim.diagnostic.goto_next, { buffer = 0 })
+  vim.keymap.set('n', '<Leader>dN', vim.diagnostic.goto_prev, { buffer = 0 })
+
+  vim.keymap.set('n', '<Leader>dt', "<cmd>Telescope diagnostics<CR>", { buffer = 0 })
+
+  vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename, { buffer = 0 })
 end
 
 
