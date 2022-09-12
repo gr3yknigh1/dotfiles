9b80aa996dc940de5391a083bd9c005834bb65f4
Ilya Akkuzin <gr3yknigh1@gmail.com>
2022-09-13 00:43:13 +0300

    [lvim] fix indentations

--- lvim/.config/nvim/lua/utils.lua
+++ lvim/.config/nvim/lua/utils.lua
@@ -6,22 +6,22 @@
 return {
 
   map = (function(modes, keys, func, opts)
-      vim.keymap.set(modes, keys, func, opts)
+    vim.keymap.set(modes, keys, func, opts)
   end),
 
   nmap = (function(keys, func, opts)
-      map("n", keys, func, opts)
+    map("n", keys, func, opts)
   end),
 
   vmap = (function(keys, func, opts)
-      map("v", keys, func, opts)
+    map("v", keys, func, opts)
   end),
 
   vnmap = (function(keys, func, opts)
-      map({"n", "v"}, keys, func, opts)
+    map({"n", "v"}, keys, func, opts)
   end),
 
   tmap = (function(keys, func, opts)
-      map("t", keys, func, opts)
+    map("t", keys, func, opts)
   end)
 }
