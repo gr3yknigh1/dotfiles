fb36dab980b1897ee44232dce008545c7dd8e5a6
Ilya Akkuzin <gr3yknigh1@gmail.com>
2022-12-24 13:40:31 +0300

    Update

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,20 +1,14 @@
 if status is-interactive
 end
 
-set EDITOR "nvim"
+alias export="set -xg"
 
-alias cls="clear"
-alias lg="lazygit"
-alias misha="gcc"
+export FISH_CONFIG_DIR "$HOME/.config/fish"
 
-alias e=$EDITOR
-
-alias cat="bat"
-
-alias ls="exa $LS_ALL_CHAR --icons --group-directories-first $argv"
-alias l1="exa $LS_ALL_CHAR -1 --icons --group-directories-first $argv"
-alias ll="exa $LS_ALL_CHAR -l --icons --group-directories-first $argv"
-alias lt="exa $LS_ALL_CHAR -T --icons --group-directories-first $argv"
+alias ls="exa --icons --group-directories-first $argv"
+alias l1="exa -1 --icons --group-directories-first $argv"
+alias ll="exa -l --icons --group-directories-first $argv"
+alias lt="exa -T --icons --group-directories-first $argv"
 
 alias t="tmux"
 alias tal="tmux attach"
@@ -23,10 +17,11 @@ alias tls="tmux ls"
 alias tkr="tmux kill-server"
 alias tks="tmux kill-session"
 
+alias e="nvim"
 alias f="ranger"
 alias v="neovide"
+alias lg="lasygit"
 
-# start x-server
 if [ $(tty) = "/dev/tty1" ]
     startx
 end
