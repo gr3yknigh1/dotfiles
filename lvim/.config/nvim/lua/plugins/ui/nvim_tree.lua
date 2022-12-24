02281df0fed90574eefdb4fa465e9c192e67d917
Ilya Akkuzin <gr3yknigh1@gmail.com>
2022-12-24 14:18:26 +0300

    Add floating mode

--- lvim/.config/nvim/lua/plugins/ui/nvim_tree.lua
+++ lvim/.config/nvim/lua/plugins/ui/nvim_tree.lua
@@ -1,10 +1,63 @@
 -- Nvim Tree
 
 local nvim_tree = require("nvim-tree")
-local utils = require("utils")
 
-local nmap = utils.nmap
+local nvim_tree_lib  = require("nvim-tree.lib")
+local nvim_tree_view = require("nvim-tree.view")
 
+local HEIGHT_RATIO = 0.8  -- You can change this
+local WIDTH_RATIO = 0.5   -- You can change this too
+
+local git_add = function()
+  local node = nvim_tree_lib.get_node_at_cursor()
+  local gs = node.git_status
+
+  -- If the file is untracked, unstaged or partially staged, we stage it
+  if gs == "??" or gs == "MM" or gs == "AM" or gs == " M" then
+    vim.cmd("silent !git add " .. node.absolute_path)
+
+  -- If the file is staged, we unstage
+  elseif gs == "M " or gs == "A " then
+    vim.cmd("silent !git restore --staged " .. node.absolute_path)
+  end
+  nvim_tree_lib.refresh_tree()
+end
+
+local function collapse_all()
+  require("nvim-tree.actions.tree-modifiers.collapse-all").fn()
+end
+
+local function edit_or_open()
+  -- open as vsplit on current node
+  local action = "edit"
+  local node = nvim_tree_lib.get_node_at_cursor()
+  -- Just copy what's done normally with vsplit
+  if node.link_to and not node.nodes then
+    require('nvim-tree.actions.node.open-file').fn(action, node.link_to)
+    nvim_tree_view.close() -- Close the tree if file was opened
+  elseif node.nodes ~= nil then
+    nvim_tree_lib.expand_or_collapse(node)
+  else
+    require('nvim-tree.actions.node.open-file').fn(action, node.absolute_path)
+    -- nvim_tree_view.close() -- Close the tree if file was opened
+  end
+end
+
+local function vsplit_preview()
+  -- open as vsplit on current node
+  local action = "vsplit"
+  local node = nvim_tree_lib.get_node_at_cursor()
+  -- Just copy what's done normally with vsplit
+  if node.link_to and not node.nodes then
+    require('nvim-tree.actions.node.open-file').fn(action, node.link_to)
+  elseif node.nodes ~= nil then
+    nvim_tree_lib.expand_or_collapse(node)
+  else
+    require('nvim-tree.actions.node.open-file').fn(action, node.absolute_path)
+  end
+  -- Finally refocus on tree if it was lost
+  nvim_tree_view.focus()
+end
 
 nvim_tree.setup({
   auto_reload_on_write = true,
@@ -24,21 +77,54 @@ nvim_tree.setup({
   sync_root_with_cwd = false,
   reload_on_bufenter = false,
   respect_buf_cwd = false,
+  tab = {
+    sync = {
+      open = true
+    }
+  },
   view = {
     adaptive_size = false,
     centralize_selection = false,
-    width = 30,
-    -- height = 30,
     hide_root_folder = false,
     side = "left",
     preserve_window_proportions = false,
     number = false,
     relativenumber = false,
     signcolumn = "yes",
+    float = {
+      enable = true,
+      open_win_config = function()
+        local screen_w = vim.opt.columns:get()
+        local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+        local window_w = screen_w * WIDTH_RATIO
+        local window_h = screen_h * HEIGHT_RATIO
+        local window_w_int = math.floor(window_w)
+        local window_h_int = math.floor(window_h)
+        local center_x = (screen_w - window_w) / 2
+        local center_y = ((vim.opt.lines:get() - window_h) / 2)
+                         - vim.opt.cmdheight:get()
+        return {
+          border = 'rounded',
+          relative = 'editor',
+          row = center_y,
+          col = center_x,
+          width = window_w_int,
+          height = window_h_int,
+        }
+        end,
+    },
+    width = math.floor(vim.opt.columns:get() * WIDTH_RATIO),
+    -- width = 30,
+    -- height = 30,
     mappings = {
       custom_only = false,
       list = {
-        -- user mappings go here
+        { key = "/", action = "live_filter" },
+        { key = "l", action = "edit", action_cb = edit_or_open },
+        { key = "L", action = "vsplit_preview", action_cb = vsplit_preview },
+        { key = "h", action = "close_node" },
+        { key = "H", action = "collapse_all", action_cb = collapse_all },
+        { key = "ga", action = "git_add", action_cb = git_add },
       },
     },
   },
@@ -141,13 +227,13 @@ nvim_tree.setup({
   actions = {
       use_system_clipboard = true,
       change_dir = {
-          enable = true,
-          global = false,
-          restrict_above_cwd = false,
+        enable = true,
+        global = false,
+        restrict_above_cwd = false,
       },
       expand_all = {
-          max_folder_discovery = 300,
-          exclude = {},
+        max_folder_discovery = 300,
+        exclude = {},
       },
       open_file = {
           quit_on_open = false,
@@ -197,5 +283,5 @@ nvim_tree.setup({
 }) -- END_DEFAULT_OPTS
 
 
-nmap("<Leader><Space>", "<cmd>NvimTreeFocus<CR>")
-nmap("<Leader><Space>", "<cmd>NvimTreeToggle<CR>")
+vim.keymap.set("n", "<Leader><Space>", "<cmd>NvimTreeFocus<CR>")
+vim.keymap.set("n", "<Leader><Space>", "<cmd>NvimTreeToggle<CR>")
