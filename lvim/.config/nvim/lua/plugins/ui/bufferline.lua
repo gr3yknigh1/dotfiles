1a308a7c5d2b53fa9bc801d0b65f61c59661188f
Ilya Akkuzin <gr3yknigh1@gmail.com>
2022-12-24 14:18:26 +0300

    Add floating mode

--- lvim/.config/nvim/lua/plugins/ui/bufferline.lua
+++ lvim/.config/nvim/lua/plugins/ui/bufferline.lua
@@ -111,21 +111,21 @@ map("n", "<Space>bl", "<Cmd>BufferOrderByLanguage<CR>", opts)
 map("n", "<Space>bw", "<Cmd>BufferOrderByWindowNumber<CR>", opts)
 
 
-local nvim_tree_events = require('nvim-tree.events')
-local bufferline_api = require('bufferline.api')
-
-local function get_tree_size()
-  return require'nvim-tree.view'.View.width
-end
-
-nvim_tree_events.subscribe('TreeOpen', function()
-  bufferline_api.set_offset(get_tree_size())
-end)
-
-nvim_tree_events.subscribe('Resize', function()
-  bufferline_api.set_offset(get_tree_size())
-end)
-
-nvim_tree_events.subscribe('TreeClose', function()
-  bufferline_api.set_offset(0)
-end)
+-- local nvim_tree_events = require('nvim-tree.events')
+-- local bufferline_api = require('bufferline.api')
+--
+-- local function get_tree_size()
+--   return require'nvim-tree.view'.View.width
+-- end
+--
+-- nvim_tree_events.subscribe('TreeOpen', function()
+--   bufferline_api.set_offset(get_tree_size())
+-- end)
+--
+-- nvim_tree_events.subscribe('Resize', function()
+--   bufferline_api.set_offset(get_tree_size())
+-- end)
+--
+-- nvim_tree_events.subscribe('TreeClose', function()
+--   bufferline_api.set_offset(0)
+-- end)
