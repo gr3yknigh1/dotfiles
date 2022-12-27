0df4aad3b7898386d9dc77b2cc9f79456095b76a
Ilya Akkuzin <gr3yknigh1@gmail.com>
2022-12-27 21:13:28 +0300

    update fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,14 +1,10 @@
 if status is-interactive
 end
 
-alias export="set -xg"
-
-export FISH_CONFIG_DIR "$HOME/.config/fish"
-
 alias ls="exa --icons --group-directories-first $argv"
 alias l1="exa -1 --icons --group-directories-first $argv"
 alias ll="exa -l --icons --group-directories-first $argv"
-alias lt="exa -T --icons --group-directories-first $argv"
+alias lt="exa -t --icons --group-directories-first $argv"
 
 alias t="tmux"
 alias tal="tmux attach"
@@ -21,6 +17,8 @@ alias e="nvim"
 alias f="ranger"
 alias v="neovide"
 alias lg="lazygit"
+
+alias pacman="pacman --color=always"
 
 if [ $(tty) = "/dev/tty1" ]
     startx
