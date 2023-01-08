09556005fbc4a0c73a33a54e2d90833614f490c2
Ilya Akkuzin <gr3yknigh1@gmail.com>
2023-01-08 18:03:17 +0300

    [fish] fix alias

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,7 +4,7 @@ end
 alias ls="exa --icons --group-directories-first $argv"
 alias l1="exa -1 --icons --group-directories-first $argv"
 alias ll="exa -l --icons --group-directories-first $argv"
-alias lt="exa -t --icons --group-directories-first $argv"
+alias lt="exa -T --icons --group-directories-first $argv"
 
 alias t="tmux"
 alias tal="tmux attach"
