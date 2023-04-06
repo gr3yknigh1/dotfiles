f1b738a3e8b65a011fdf3db7fdb0a5270b5698ca
Ilya Akkuzin <gr3yknigh1@gmail.com>
2023-04-06 19:16:25 +0300

    [lvim] Configure `gitsigns`

--- development/.config/nvim/lua/plugins/install.lua
+++ development/.config/nvim/lua/plugins/install.lua
@@ -25,7 +25,6 @@ end
 local packer_bootstrap = ensure_packer()
 
 require('packer').startup(function(use)
-
   use 'wbthomason/packer.nvim'
 
   -- Colorschemes
@@ -41,7 +40,7 @@ require('packer').startup(function(use)
   use 'jayp0521/mason-nvim-dap.nvim'
 
   use 'mfussenegger/nvim-dap'
-  use { 'rcarriga/nvim-dap-ui', requires = {'mfussenegger/nvim-dap'} }
+  use { 'rcarriga/nvim-dap-ui', requires = { 'mfussenegger/nvim-dap' } }
   use 'theHamsta/nvim-dap-virtual-text'
 
   use 'nvim-treesitter/nvim-treesitter'
@@ -70,7 +69,7 @@ require('packer').startup(function(use)
   -- Tools
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.0',
-    requires = { {'nvim-lua/plenary.nvim'} }
+    requires = { { 'nvim-lua/plenary.nvim' } }
   }
   use 'kdheepak/lazygit.nvim'
 
@@ -89,43 +88,127 @@ require('packer').startup(function(use)
   use 'gpanders/editorconfig.nvim'
   use 'kyazdani42/nvim-web-devicons'
   use 'stevearc/vim-arduino'
-  use 'lewis6991/gitsigns.nvim'
+  use {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup({
+        signs                        = {
+          add          = { text = '│' },
+          change       = { text = '│' },
+          delete       = { text = '_' },
+          topdelete    = { text = '‾' },
+          changedelete = { text = '~' },
+          untracked    = { text = '┆' },
+        },
+        signcolumn                   = true,  -- Toggle with `:Gitsigns toggle_signs`
+        numhl                        = false, -- Toggle with `:Gitsigns toggle_numhl`
+        linehl                       = false, -- Toggle with `:Gitsigns toggle_linehl`
+        word_diff                    = false, -- Toggle with `:Gitsigns toggle_word_diff`
+        watch_gitdir                 = {
+          interval = 1000,
+          follow_files = true
+        },
+        attach_to_untracked          = true,
+        current_line_blame           = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+        current_line_blame_opts      = {
+          virt_text = true,
+          virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+          delay = 1000,
+          ignore_whitespace = false,
+        },
+        current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+        sign_priority                = 6,
+        update_debounce              = 100,
+        status_formatter             = nil,   -- Use default
+        max_file_length              = 40000, -- Disable if file is longer than this (in lines)
+        preview_config               = {
+          -- Options passed to nvim_open_win
+          border = 'single',
+          style = 'minimal',
+          relative = 'cursor',
+          row = 0,
+          col = 1
+        },
+        yadm                         = {
+          enable = false
+        },
+        on_attach                    = function(bufnr)
+          local gs = package.loaded.gitsigns
+
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+
+          -- Navigation
+          map('n', ']c', function()
+            if vim.wo.diff then return ']c' end
+            vim.schedule(function() gs.next_hunk() end)
+            return '<Ignore>'
+          end, { expr = true })
+
+          map('n', '[c', function()
+            if vim.wo.diff then return '[c' end
+            vim.schedule(function() gs.prev_hunk() end)
+            return '<Ignore>'
+          end, { expr = true })
+
+          -- Actions
+          map({ 'n', 'v' }, '<leader>hs', ':Gitsigns stage_hunk<CR>')
+          map({ 'n', 'v' }, '<leader>hr', ':Gitsigns reset_hunk<CR>')
+          map('n', '<leader>hS', gs.stage_buffer)
+          map('n', '<leader>hu', gs.undo_stage_hunk)
+          map('n', '<leader>hR', gs.reset_buffer)
+          map('n', '<leader>hp', gs.preview_hunk)
+          map('n', '<leader>hb', function() gs.blame_line { full = true } end)
+          map('n', '<leader>tb', gs.toggle_current_line_blame)
+          map('n', '<leader>hd', gs.diffthis)
+          map('n', '<leader>hD', function() gs.diffthis('~') end)
+          map('n', '<leader>td', gs.toggle_deleted)
+
+          -- Text object
+          map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+        end
+      })
+    end
+  }
+  use 'skywind3000/vim-cppman'
+
   use {
     'windwp/nvim-autopairs',
     config = function()
       require('nvim-autopairs').setup({
         disable_filetype = { "TelescopePrompt", "spectre_panel" },
-        disable_in_macro = false, -- disable when recording or executing a macro
+        disable_in_macro = false,       -- disable when recording or executing a macro
         disable_in_visualblock = false, -- disable when insert after visual block mode
         disable_in_replace_mode = true,
         ignored_next_char = [=[[%w%%%'%[%"%.%`%$]]=],
         enable_moveright = true,
-        enable_afterquote = true , -- add bracket pairs after quote
-        enable_check_bracket_line = true , --- check bracket in same line
-        enable_bracket_in_quote = true, --
-        enable_abbr = false, -- trigger abbreviation
-        break_undo = true, -- switch for basic rule break undo sequence
+        enable_afterquote = true,         -- add bracket pairs after quote
+        enable_check_bracket_line = true, --- check bracket in same line
+        enable_bracket_in_quote = true,   --
+        enable_abbr = false,              -- trigger abbreviation
+        break_undo = true,                -- switch for basic rule break undo sequence
         check_ts = false,
         map_cr = true,
-        map_bs = true , -- map the <BS> key
-        map_c_h = false , -- Map the <C-h> key to delete a pair
+        map_bs = true,   -- map the <BS> key
+        map_c_h = false, -- Map the <C-h> key to delete a pair
         map_c_w = false, -- map <c-w> to delete a pair if possible
       })
     end
   }
 
   use({
-      'kylechui/nvim-surround',
-      tag = '*',
-      config = function()
-          require('nvim-surround').setup({
-          })
-      end
+    'kylechui/nvim-surround',
+    tag = '*',
+    config = function()
+      require('nvim-surround').setup({
+      })
+    end
   })
 
   if packer_bootstrap then
     require('packer').sync()
   end
-
 end)
-
