73512a2bdc7a2ebb979c76ac090c293925e90d50
Ilya Akkuzin <gr3yknigh1@gmail.com>
2023-05-10 21:39:03 +0300

    emacs: Update

--- arch-development/.emacs.d/init.el
+++ arch-development/.emacs.d/init.el
@@ -5,7 +5,7 @@
 (set-face-attribute 'default nil :font "JetBrainsMono Nerd Font-14")
 
 ;; Maximize screen on startup
-(add-hook 'window-setup-hook 'toggle-frame--maximized t)
+(add-hook 'Window-setup-hook 'toggle-frame--maximized t)
 
 ;; Disable menu on startup
 (menu-bar-mode -1)
@@ -20,6 +20,7 @@
 (setq c-default-style "linux"
       c-basic-offset 4)
 
+;; Themes
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -34,3 +35,54 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(load-theme 'gruvbox-light-medium t)
+
+;; Something. Idk
+(electric-pair-mode 1)
+
+;; Display whitespace
+;; (global-whitespace-mode)
+
+;; Copy and paste
+(cua-mode t)
+(setq cua-auto-tabify-rectangles nil) ;; Don't tabify after rectangle commands
+(transient-mark-mode 1)               ;; No region when it is not highlighted
+(setq cua-keep-region-after-copy t)   ;; Standard Windows behaviour(global-cua-moded)
+
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(package-initialize)
+
+(setq package-selected-packages '(lsp-mode yasnippet lsp-treemacs helm-lsp
+    projectile hydra flycheck company avy which-key helm-xref dap-mode))
+
+(when (cl-find-if-not #'package-installed-p package-selected-packages)
+  (package-refresh-contents)
+  (mapc #'package-install package-selected-packages))
+
+;; sample `helm' configuration use https://github.com/emacs-helm/helm/ for details
+(helm-mode)
+(require 'helm-xref)
+(define-key global-map [remap find-file] #'helm-find-files)
+(define-key global-map [remap execute-extended-command] #'helm-M-x)
+(define-key global-map [remap switch-to-buffer] #'helm-mini)
+
+(which-key-mode)
+(add-hook 'c-mode-hook 'lsp)
+(add-hook 'c++-mode-hook 'lsp)
+
+(setq gc-cons-threshold (* 100 1024 1024)
+      read-process-output-max (* 1024 1024)
+      treemacs-space-between-root-nodes nil
+      company-idle-delay 0.0
+      company-minimum-prefix-length 1
+      lsp-idle-delay 0.1)  ;; clangd is fast
+
+(with-eval-after-load 'lsp-mode
+  (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
+  (require 'dap-cpptools)
+  (yas-global-mode))
+
+(setq lsp-keymap-prefix "\\")
