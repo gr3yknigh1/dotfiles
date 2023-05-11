5758e1c3ba2feb5684c46df4a61cfc0a9ba912a4
Ilya Akkuzin <i.akkuzin@avsw.ru>
2023-05-11 09:03:47 +0300

    work

---
+++ deb-work-machine/.config/nvim/lua/core/init.lua
@@ -0,0 +1,8 @@
+-- lua/core/init.lua
+-- core config
+
+require('core.settings')
+require('core.keymaps')
+require('core.apperience')
+require('core.neovide')
+
---
+++ deb-work-machine/.config/nvim/lua/plugins/init.lua
@@ -0,0 +1,8 @@
+-- lua/plugins/init.lua
+
+require('plugins.install')
+require('plugins.tools')
+require('plugins.mason')
+require('plugins.ui')
+require('plugins.lsp')
+-- require('plugins.dap')
