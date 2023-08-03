6742cda66f7ce35a97fcbd72ae70c6e2d63ee32c
Ilya Akkuzin <gr3yknigh1@gmail.com>
2023-08-03 21:52:23 +0300

    Remove unused pkgs

--- stash/xorg-i3-desktop/.local/bin/start-webcam.sh
+++
@@ -1,2 +0,0 @@
-# https://gist.github.com/jessarcher/39fbabd9864077a0a1349c24c8277af5
-gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video2
