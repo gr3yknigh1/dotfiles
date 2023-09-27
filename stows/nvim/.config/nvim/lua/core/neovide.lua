79a7ffde19063b9b46488faf1c6c65ff85a9f644
Ilya Akkuzin <gr3yknigh1@gmail.com>
2023-09-27 14:07:50 +0300

    nvim: neovide: update

--- stows/nvim/.config/nvim/lua/core/neovide.lua
+++ stows/nvim/.config/nvim/lua/core/neovide.lua
@@ -9,15 +9,16 @@ if vim.fn.exists('g:neovide') and vim.g.neovide then
     return font_name .. ':h' .. tostring(font_size)
   end
 
-  local font_name         = 'Iosevka Nerd Font'
-  local default_font_size = 9
+  -- local font_name         = 'Iosevka Nerd Font'
+  local font_name         = 'JetBrainsMono Nerd Font'
+  local default_font_size = 20
   local font_size         = default_font_size
 
   vim.opt.guifont = get_guifont(font_name, font_size)
 
   vim.g.neovide_scale_factor = 1.0
-  vim.g.neovide_transparency = 1
-  vim.g.transparency         = 1
+  vim.g.neovide_transparency = 0.9
+  vim.g.transparency         = 0.9
 
   -- vim.g.neovide_background_color = '#0f1117'.printf('%x', float2nr(255 * g:transparency))
 
@@ -35,7 +36,7 @@ if vim.fn.exists('g:neovide') and vim.g.neovide then
   --   - sonicboom
   --   - ripple
   --   - wireframe
-  vim.g.neovide_cursor_vfx_mode         = 'ripple'
+  vim.g.neovide_cursor_vfx_mode         = 'sonicboom'
 
   vim.keymap.set('n', '<C-=>', function()
     font_size = font_size + 1;
