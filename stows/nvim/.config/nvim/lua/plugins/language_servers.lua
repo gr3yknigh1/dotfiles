8c4694131023a128a84874ea955b494fb9eeea99
Ilya Akkuzin <gr3yknigh1@gmail.com>
2023-12-21 22:16:50 +0300

    nvim: Add zls

--- stows/nvim/.config/nvim/lua/plugins/language_servers.lua
+++ stows/nvim/.config/nvim/lua/plugins/language_servers.lua
@@ -1,3 +1,3 @@
 return {
-  'pyright', 'clangd', 'cmake', 'lua_ls', 'gopls', 'rust_analyzer', 'csharp_ls', 'asm_lsp'
+  'pyright', 'clangd', 'cmake', 'lua_ls', 'gopls', 'rust_analyzer', 'csharp_ls', 'asm_lsp', 'zls'
 }
