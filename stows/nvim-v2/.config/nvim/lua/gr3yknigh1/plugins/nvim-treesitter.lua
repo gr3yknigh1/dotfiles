65fb9d64a815f51c4c151a3df065a4da5b001951
Ilya Akkuzin <i.akkuzin@avsw.ru>
2024-02-26 09:47:46 +0300

    nvim wip: Trying to solve issue with nvim-treesitter parser installation
    Still doesn't working

--- stows/nvim-v2/.config/nvim/lua/gr3yknigh1/plugins/nvim-treesitter.lua
+++ stows/nvim-v2/.config/nvim/lua/gr3yknigh1/plugins/nvim-treesitter.lua
@@ -8,26 +8,27 @@ return {
     -- <https://github.com/nvim-treesitter/nvim-treesitter/issues/3605#issuecomment-1644591006>
     local parser_install_dir = vim.fn.stdpath("data") .. "/treesitter"
 
-    vim.opt.runtimepath:append(parser_install_dir)
+    -- vim.opt.runtimepath:append(parser_install_dir)
 
-    for _, v in ipairs(vim.opt.runtimepath:get()) do
-      if (string.match(v, "lazy/nvim%-treesitter")) then
-        vim.opt.runtimepath:remove(v)
-      end
-    end
+    -- for _, v in ipairs(vim.opt.runtimepath:get()) do
+    --   if (string.match(v, "lazy/nvim%-treesitter")) then
+    --     vim.opt.runtimepath:remove(v)
+    --   end
+    -- end
 
     configs.setup({
-      ensure_installed = "all",
+      -- ensure_installed = "all",
       sync_install = false,
       auto_install = false,
       highlight = {
         enable = true,
+        disable = {},
       },
-      additional_vim_regex_highlighting = false,
+      additional_vim_regex_highlighting = true,
       indent = {
         enable = true
       },
-      parser_install_dir = parser_install_dir,
+      -- parser_install_dir = parser_install_dir,
     })
   end
 }
